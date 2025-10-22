827dbb349e222139766d7ec027c358f7469aa1ce
Gerardo Paredes <gerardparedes23@gmail.com>
2025-10-21 22:19:31 -0700

    setup apollov4 codegen

--- apps/client/src/graphql/queries/hello.ts
+++ apps/client/src/graphql/queries/hello.ts
@@ -1,7 +1,20 @@
+import { type TypedDocumentNode } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+import {
+  type HelloQuery,
+  type HelloQueryVariables,
+} from "../__generated__/graphql";
+
 import { gql } from "@apollo/client";
 
-export const HELLO_QUERY = gql`
+const HELLO_QUERY: TypedDocumentNode<HelloQuery, HelloQueryVariables> = gql`
   query Hello($name: String) {
     hello(name: $name)
   }
 `;
+
+export const useHelloQuery = (variables: HelloQueryVariables) => {
+  return useQuery<HelloQuery, HelloQueryVariables>(HELLO_QUERY, {
+    variables,
+  });
+};
